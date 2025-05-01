693e99497989a50cfab6a871c1c72d4f87caa243
Syed <amanxsyed@gmail.com>
2025-05-01 23:29:29 +0500

    Add responsive sidebar menu to Navbar component

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/frontend_assets/assets.js'
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} alt="logo" className='w-36' />
@@ -42,6 +44,22 @@ const Navbar = () => {
                 <img src={assets.cart_icon} alt="cart" className='w-5 min-w-5 cursor-pointer' />
                 <p className='absolute right-[-5px] bottom-[-5px] w-4 leading-4 text-center bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
             </Link>
+            <img onClick={()=> setVisible(true)} src={assets.menu_icon} alt="menu" className='w-5 cursor-pointer sm:hidden' />
+        </div>
+
+        {/* side bar menu for small screens */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all 
+        ${visible ? 'w-full' : 'w-0'} `} >
+            <div className='flex flex-col text-gray-600'>
+                <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img src={assets.dropdown_icon} alt="" className='h-4 rotate-180'/>
+                    <p>Back</p>
+                </div>
+                <NavLink className='py-2 pl-6 border' to="/" >HOME</NavLink>
+                <NavLink className='py-2 pl-6 border' to="/collection" >COLLECTION</NavLink>
+                <NavLink className='py-2 pl-6 border' to="/about" >ABOUT</NavLink>
+                <NavLink className='py-2 pl-6 border' to="/contact" >CONTACT</NavLink>
+            </div>
         </div>
     </div>
   )
